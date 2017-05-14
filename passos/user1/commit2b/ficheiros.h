9136ab270d2bddb4d56a5970a26ee7d61f9040e5
Bernardo Vieira <bernardo5@live.com.pt>
2017-05-14 01:03:39 +0100

    commit2b & pptx

---
+++ passos/user1/commit2b/ficheiros.h
@@ -0,0 +1,3 @@
+
+struct pergunta_* carregar_ficheiro_perguntas();
+int guardar_ficheiro_perguntas(struct pergunta_ pergunta);
---
+++ passos/user1/commit2b/perguntas.c
@@ -0,0 +1,7 @@
+
+#include "perguntas.h"
+
+void obter_perguntas_aleatorias(struct pergunta_ perguntas[7])
+{
+
+}
